76f6227b73895998a2ac5a4d495b5fd1404362f3
fred077a <fred18q3@stud.kea.dk>
2025-01-27 22:44:03 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LearningDotNet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
