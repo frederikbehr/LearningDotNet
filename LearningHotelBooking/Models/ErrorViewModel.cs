98cbbbf7095655dc3c71109222125c3808182a80
fred077a <fred18q3@stud.kea.dk>
2025-01-28 13:38:47 +0100

    added new project

---
+++ LearningHotelBooking/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LearningHotelBooking.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
