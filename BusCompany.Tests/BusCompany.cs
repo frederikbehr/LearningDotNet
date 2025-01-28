dc3511f6d415148945140a0f95a358058378b916
fred077a <fred18q3@stud.kea.dk>
2025-01-28 12:28:08 +0100

    Restructure of project and tests added

---
+++ BusCompany.Tests/BusCompany.cs
@@ -0,0 +1,64 @@
+﻿using Xunit;
+
+namespace BusCompany.Tests
+{
+    public class BusCompanyTests
+    {
+        [Fact] // This attribute indicates a test method
+        public void GetSmallPrice()
+        {
+            //Instance
+            BusCompanyManager busCompany = new BusCompanyManager();
+
+            //Set distance
+            busCompany.SetDistance(50);
+            double price = busCompany.CalculateCost();
+            
+            // Assert
+            Assert.Equal(50*10+2500, price);
+        }
+        
+        [Fact] // This attribute indicates a test method
+        public void GetMediumPrice()
+        {
+            //Instance
+            BusCompanyManager busCompany = new BusCompanyManager();
+
+            //Set distance
+            busCompany.SetDistance(400);
+            double price = busCompany.CalculateCost();
+            
+            // Assert
+            Assert.Equal(400*8+2500, price);
+        }
+        
+        [Fact] // This attribute indicates a test method
+        public void GetMediumBorderPrice()
+        {
+            //Instance
+            BusCompanyManager busCompany = new BusCompanyManager();
+
+            //Set distance
+            busCompany.SetDistance(500);
+            double price = busCompany.CalculateCost();
+            
+            // Assert
+            Assert.Equal(500*6+2500, price);
+        }
+        
+        [Fact] // This attribute indicates a test method
+        public void GetBigPrice()
+        {
+            //Instance
+            BusCompanyManager busCompany = new BusCompanyManager();
+
+            //Set distance
+            busCompany.SetDistance(1000);
+            double price = busCompany.CalculateCost();
+            
+            // Assert
+            Assert.Equal(1000*6+2500, price);
+        }
+    }
+
+}
